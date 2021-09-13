7dcfd3c464347b3fd34abc34488783d49c8cb142
JettaByte <64786269+JettaByte@users.noreply.github.com>
2021-09-13 16:40:03 +0900

    [ ⛓ | 2109301 ] [ Fixed action message ]
    [ ⛓ | 2109301 ] [ Fixed action message ]

--- APP.py
+++ APP.py
@@ -5,19 +5,15 @@ import discord
 from discord import Webhook, RequestsWebhookAdapter
 from discord.ext.commands import bot
 
-webhooks = input("웹후크 주소를 입력해주세요")
-tokens = input("봇 토큰을 입력해주세요")
+webhooks = input("Please enter the webhook address | ")
+tokens = input("Please enter your bot token | ")
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print('감시 시작')
-    print('다음 봇에서 감시중 입니다.')
-    print(client.user.name)
-    print(client.user.id)
-    print('--------------')
+    print('Start the watch process')
 
 
 @client.event
@@ -25,11 +21,10 @@ async def on_message(message):
     webhook = Webhook.from_url(
         webhooks,
         adapter=RequestsWebhookAdapter())
-    webhook.send("감시 대상에서 새로운 메세지가 발송됨.")
-    webhook.send("해당 메세지 내용:")
+    webhook.send("You have confirmed that a new message has been sent.")
     webhook.send(message)
     webhook.send(message.content)
-    print('감시 대상에서 새로운 메세지가 발송됨.')
+    print('You have confirmed that a new message has been sent.')
     print(message)
     print(message.content)
 
